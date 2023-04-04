63c9c2f882f4206c2ab694d276e5425b76bca738
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-04-04 19:26:14 +0600

    index page and top bar responsiveness issue solved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,8 +10,8 @@ const nunito = Nunito_Sans({ subsets: ['latin'], weight: '400' });
 export default function App({ Component, pageProps }) {
   return (
     <main className={nunito.className}>
-      <div className='flex justify-center py-3 px-5 shadow max-w-[85rem] mx-auto'>
-        <Link href='/' className='font-bold text-center text-3xl cursor-pointer hover:underline grow'>Tech Blogs</Link>
+      <div className='flex justify-between lg:justify-center py-3 px-3 lg:px-5 shadow max-w-[85rem] mx-auto'>
+        <Link href='/' className='font-bold text-center text-2xl lg:text-3xl cursor-pointer hover:underline lg:grow'>Tech Blogs</Link>
         <button
           onClick={()=>openModal(<LoginForm/>)}
           className='font-bold inline-flex items-center gap-1 active:scale-95 bg-gray-950 rounded text-white px-3'>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,19 +15,19 @@ export default function Home() {
 
       <section className='flex flex-wrap gap-5 justify-center max-w-6xl mx-auto px-3 pt-5 sm:pt-10 pb-5'>
         
-        <div className='basis-full flex flex-wrap items-center gap-5 justify-between'>
+        <div className='basis-full flex flex-col lg:flex-row lg:items-center gap-5 justify-between'>
         
-          <label htmlFor="myPost" className='inline-flex gap-2 items-center pt-1 cursor-pointer '>
+          <label htmlFor="myPost" className='inline-flex gap-2 items-center pt-1 cursor-pointer order-2 lg:order-none'>
             <input type="checkbox" className='h-5 w-5' name="" id="myPost" />
-            <span className='font-bold select-none'>Show My Post</span>
+            <span className='font-bold select-none'>My Blogs</span>
           </label>
 
-          <div className='inline-flex rounded overflow-hidden grow max-w-sm'>
+          <div className='inline-flex rounded overflow-hidden grow lg:max-w-sm order-1 lg:order-none'>
             <span className='font-bold tracking-wider px-2 bg-gray-900 leading-9 text-white'>Search</span>
             <input type="text" className='min-w-0 w-full p-1.5 border-2 border-gray-600 font-bold text-gray-600' name=""/>
           </div>
 
-          <Link href='/create-blog' className='inline-flex items-center hover:underline decoration-2 underline-offset-2'>
+          <Link href='/create-blog' className='inline-flex items-center hover:underline decoration-2 underline-offset-2 order-2 lg:order-none'>
             <IoCreate className='w-8 h-8'/>
             <span className='tracking-widest font-bold pt-1'>Create a New Blog</span>
           </Link>
