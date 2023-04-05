a5a0a13168811f840912518ac77207695f9dca04
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-04-05 17:51:13 +0600

    login modal converted to login route for require auth implementation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,3 @@
-import LoginForm from '@/components/LoginForm';
-import Modal, { openModal } from '@/components/Modal';
 import '@/styles/globals.css';
 import { Nunito_Sans } from "next/font/google";
 import Link from 'next/link';
@@ -12,15 +10,14 @@ export default function App({ Component, pageProps }) {
     <main className={nunito.className}>
       <div className='flex justify-between lg:justify-center py-3 px-3 lg:px-5 shadow max-w-[85rem] mx-auto'>
         <Link href='/' className='font-bold text-center text-2xl lg:text-3xl cursor-pointer hover:underline lg:grow'>Tech Blogs</Link>
-        <button
-          onClick={()=>openModal(<LoginForm/>)}
+        <Link
+          href={'/login'}
           className='font-bold inline-flex items-center gap-1 active:scale-95 bg-gray-950 rounded text-white px-3'>
           <BiLogIn />
           <span className='tracking-wider'>Login</span>
-        </button>
+        </Link>
       </div>
       <Component {...pageProps} />
-      <Modal />
     </main>
   )
 }
